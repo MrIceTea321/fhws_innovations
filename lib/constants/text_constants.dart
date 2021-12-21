11431306455581c9ae18c2da9588a758f7f06b15
Robert Schneider <63114586+EhrenbruderKaido@users.noreply.github.com>
2021-12-21 17:02:11 +0100

    delete constant

--- lib/constants/text_constants.dart
+++ lib/constants/text_constants.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 const String contractAddress = '0x81a4F098799e0D52af2B1FF8fD7e21f019fA268c';
 const String contractName = 'FhwsInnovationsContract';
-const String myAddress = "0x33398a5f4372DDB334aFE22d777A351b610a930C";
 const String openSansFontFamily = 'Open Sans';
 const fhwsGreen = Color.fromRGBO(143, 212, 0, 1.0);
 const String fhwsApi = 'https://apistaging.fiw.fhws.de/auth/api/users/me';
