3f8cc0c237f0384ea2922acb77b48cf0d80d1782
Robert Schneider <63114586+EhrenbruderKaido@users.noreply.github.com>
2021-12-30 18:30:04 +0100

    new contract

--- lib/constants/text_constants.dart
+++ lib/constants/text_constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String contractAddress = '0x81a4F098799e0D52af2B1FF8fD7e21f019fA268c';
+const String contractAddress = '0x79681Fb0aF5ceD3A62a6E095a51e9552EbdC3135';
 const String contractName = 'FhwsInnovationsContract';
 const fhwsGreen = Color.fromRGBO(143, 212, 0, 1.0);
 const String fhwsApi = 'https://apistaging.fiw.fhws.de/auth/api/users/me';
