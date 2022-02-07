03b351b8b79c3b9366589057a67f5b6d65f1e85f
Robert Schneider <63114586+EhrenbruderKaido@users.noreply.github.com>
2022-02-07 17:31:53 +0100

    Updatet Smart Contract

--- FhwsInnovationsContract.sol
+++ FhwsInnovationsContract.sol
@@ -19,9 +19,11 @@ contract FhwsInnovationsContract {
         string description;
     }
 
+    //owner of smart contract
     address private owner;
-
-    bool public innovationProcessFinished;
+    
+    //check if innovation process is finished
+    bool private innovationProcessFinished;
 
     //search with registred address for student
     mapping(address => Student) private students;
@@ -34,6 +36,7 @@ contract FhwsInnovationsContract {
 
     //all created innovations
     Innovation[] private innovations;
+    
     string[] private kNumbers;
     address[] private addresses;
     Innovation[] private winner;
@@ -42,19 +45,27 @@ contract FhwsInnovationsContract {
         owner = msg.sender;
     }
 
-    modifier onylWithRegistredStudentAddress {
-        require(studentUsesInitialRegistredAddress(), "You need to select your registred Wallet or register yourself with your kNumber.");
+    modifier onlyWithRegistredStudentAddress {
+        require(studentUsesInitialRegistredAddress(), "You need to register yourself with your kNumber or select your registred Wallet!");
         _;
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner, "Only owner can end the innovaton process!");
+        require(msg.sender == owner, "Only the owner can end the innovaton process!");
         _;
     }
 
-    modifier innovationProcessIsFinished {
-        require(innovationProcessFinished, "The innovation process is finished.");
+    modifier isInnovationProcessFinished {
+        require(innovationProcessFinished, "The innovation process is not finished.");
         _;
+    }
+
+    function getWinnerOfInnovationProcess() public isInnovationProcessFinished view returns(Innovation[] memory) {
+        return winner;
+    }
+
+    function getContractOwner() public view returns(address) {
+        return owner;
     }
 
     function endInnovationProcess() public onlyOwner returns(Innovation[] memory)  {
@@ -70,18 +81,11 @@ contract FhwsInnovationsContract {
                 winner.push(innovations[i]);
             }
         }
+        console.log("We have a winner!");
         return winner;
     }
 
-    function getWinnerOfInnovationProcess() public innovationProcessIsFinished view returns(Innovation[] memory) {
-        return winner;
-    }
-
-    function getContractOwner() public view returns(address) {
-        return owner;
-    }
-
-    function restartInnovationProcess() public onlyOwner innovationProcessIsFinished {
+    function restartInnovationProcess() public onlyOwner isInnovationProcessFinished {
         innovationProcessFinished = false;
         for(uint i = 0; i<kNumbers.length; i++) {
             delete kNumberAlreadyRegistred[kNumbers[i]];
@@ -94,6 +98,7 @@ contract FhwsInnovationsContract {
         delete kNumbers;
         delete addresses;
         delete winner;
+        console.log("Innovation process restarted!");
     }
 
     //registration process
@@ -108,41 +113,45 @@ contract FhwsInnovationsContract {
         } else console.log("Student already exists");
     }
 
-    //check if student is already registred
     function studentAlreadyRegistred(string memory _kNumber) public view returns(bool){
         return kNumberAlreadyRegistred[_kNumber];
     }
 
-    //check if student uses initial registred address
     function studentUsesInitialRegistredAddress() public view returns(bool){
         return students[msg.sender].studentAddress == msg.sender;
     }
 
-    //get kNumber with student address
-    function getKNumberOfStudentAddress() public onylWithRegistredStudentAddress view returns(string memory) {
+    function getKNumberOfStudentAddress() public onlyWithRegistredStudentAddress view returns(string memory) {
         return students[msg.sender].kNumber;
     }
 
-    //return student object
-    function getStudent(address studentAddress) public onylWithRegistredStudentAddress view returns(Student memory) {
+    function getStudent(address studentAddress) public onlyWithRegistredStudentAddress view returns(Student memory) {
         return students[studentAddress];
     }
 
-    //return innovations of student 
-    function getInnovationsOfStudent(address studentAddress) public onylWithRegistredStudentAddress view returns(Innovation[] memory){
+    function getInnovationsOfStudent(address studentAddress) public onlyWithRegistredStudentAddress view returns(Innovation[] memory){
          return innovationsOfStudent[studentAddress];
     }
 
-    //return all innovations
-     function getAllInnovations() public onylWithRegistredStudentAddress view returns(Innovation[] memory){
+    function getAllInnovations() public onlyWithRegistredStudentAddress view returns(Innovation[] memory){
         return innovations;
     }
 
-     function genereateUniqueHashForInnovation(string memory _title, string memory _description) private onylWithRegistredStudentAddress view returns(bytes32) {
+    //unique hash to identify innovation
+     function genereateUniqueHashForInnovation(string memory _title, string memory _description) private onlyWithRegistredStudentAddress view returns(bytes32) {
         return keccak256(abi.encodePacked(block.timestamp, getKNumberOfStudentAddress(), _title, _description));
     }
 
-    function createInnovation(string memory _title, string memory _description) public onylWithRegistredStudentAddress { 
+    function giveStudentsVoteBack(bytes32 _uniqueInnovationHash, uint index) private onlyWithRegistredStudentAddress{
+        for(uint z = 0; z< addresses.length; z++){
+            if(students[addresses[index]].votedInnovationHash == _uniqueInnovationHash){
+                students[addresses[index]].voted = false;
+                delete students[addresses[index]].votedInnovationHash;
+            } 
+        }
+    }
+
+    function createInnovation(string memory _title, string memory _description) public onlyWithRegistredStudentAddress { 
         bytes32 _uniqueInnovationHash = genereateUniqueHashForInnovation(_title, _description);
         Innovation memory newInnovation = Innovation ({uniqueInnovationHash: _uniqueInnovationHash, votingCount: 0, creator: getStudent(msg.sender), title: _title, description: _description});
         innovations.push(newInnovation);
@@ -150,18 +159,14 @@ contract FhwsInnovationsContract {
         console.log("Innovation created");
     }
 
-    function deleteInnovation(bytes32 _uniqueInnovationHash) public onylWithRegistredStudentAddress {
+    function deleteInnovation(bytes32 _uniqueInnovationHash) public onlyWithRegistredStudentAddress {
+        uint check = 0;
         for(uint i = 0; i < innovations.length; i++) {
             if(innovations[i].uniqueInnovationHash ==_uniqueInnovationHash){
-                //give students their vote back
-                for(uint z = 0; z< addresses.length; z++){
-                    if(students[addresses[i]].votedInnovationHash == _uniqueInnovationHash){
-                        students[addresses[i]].voted = false;
-                        delete students[addresses[i]].votedInnovationHash;
-                    } 
-                }
+                giveStudentsVoteBack(_uniqueInnovationHash, i);
                 innovations[i]=innovations[innovations.length-1];
                 innovations.pop();
+                check++;
             }
         }
 
@@ -169,16 +174,21 @@ contract FhwsInnovationsContract {
             if(innovationsOfStudent[msg.sender][i].uniqueInnovationHash ==_uniqueInnovationHash){
                 innovationsOfStudent[msg.sender][i]=innovationsOfStudent[msg.sender][innovationsOfStudent[msg.sender].length-1];
                 innovationsOfStudent[msg.sender].pop();
+                check++;
             }
         }
-        console.log("Innovation deleted");
+        if(check==2) console.log("Innovation deleted");
+        else console.log("The innovation with the specified hash does not exist");
     }
 
-    function editInnovation(bytes32 _uniqueInnovationHash, string memory _title, string memory _description) public onylWithRegistredStudentAddress{
+    function editInnovation(bytes32 _uniqueInnovationHash, string memory _title, string memory _description) public onlyWithRegistredStudentAddress{
+        uint check = 0;
         for(uint i = 0; i < innovations.length; i++) {
             if(innovations[i].uniqueInnovationHash ==_uniqueInnovationHash){
+                giveStudentsVoteBack(_uniqueInnovationHash, i);
                 innovations[i].title = _title;
                 innovations[i].description = _description;
+                check++;
             }
         }
 
@@ -186,39 +196,56 @@ contract FhwsInnovationsContract {
             if(innovationsOfStudent[msg.sender][i].uniqueInnovationHash ==_uniqueInnovationHash){
                 innovationsOfStudent[msg.sender][i].title = _title;
                 innovationsOfStudent[msg.sender][i].description = _description;
+                check++;
             }
         }
+        if(check==2) console.log("Innovation edited");
+        else console.log("The innovation with the specified hash does not exist");
     }
 
-
-    function vote(bytes32 _uniqueInnovationHash) public onylWithRegistredStudentAddress{
-        for(uint i = 0; i < innovations.length; i++) {
-            if(innovations[i].uniqueInnovationHash ==_uniqueInnovationHash){
-                innovations[i].votingCount++;
+    function vote(bytes32 _uniqueInnovationHash) public onlyWithRegistredStudentAddress{
+        if(!students[msg.sender].voted){
+            uint check = 0;
+            for(uint i = 0; i < innovations.length; i++) {
+                if(innovations[i].uniqueInnovationHash ==_uniqueInnovationHash){
+                    innovations[i].votingCount++;
+                    check++;
+                }
             }
-        }
 
-         for(uint i = 0; i<innovationsOfStudent[msg.sender].length; i++){
-            if(innovationsOfStudent[msg.sender][i].uniqueInnovationHash ==_uniqueInnovationHash){
-                innovationsOfStudent[msg.sender][i].votingCount++;
+            for(uint i = 0; i<innovationsOfStudent[msg.sender].length; i++){
+                if(innovationsOfStudent[msg.sender][i].uniqueInnovationHash ==_uniqueInnovationHash){
+                    innovationsOfStudent[msg.sender][i].votingCount++;
+                    check++;
+                }
             }
-        }
-        students[msg.sender].voted = true;
-        students[msg.sender].votedInnovationHash = _uniqueInnovationHash;
+            students[msg.sender].voted = true;
+            students[msg.sender].votedInnovationHash = _uniqueInnovationHash;
+            
+            if(check==2) console.log("Student voted");
+            else console.log("The innovation with the specified hash does not exist");
+        } else console.log("Student already voted");
     }
 
-    function unvote(bytes32 _uniqueInnovationHash) public onylWithRegistredStudentAddress{
-        for(uint i = 0; i < innovations.length; i++) {
-            if(innovations[i].uniqueInnovationHash ==_uniqueInnovationHash){
-                innovations[i].votingCount--;
+    function unvote(bytes32 _uniqueInnovationHash) public onlyWithRegistredStudentAddress{
+        if(students[msg.sender].voted){    
+            uint check = 0;
+            for(uint i = 0; i < innovations.length; i++) {
+                if(innovations[i].uniqueInnovationHash ==_uniqueInnovationHash){
+                    innovations[i].votingCount--;
+                    check++;
+                }
             }
-        }
-        for(uint i = 0; i<innovationsOfStudent[msg.sender].length; i++){
-            if(innovationsOfStudent[msg.sender][i].uniqueInnovationHash ==_uniqueInnovationHash){
-                innovationsOfStudent[msg.sender][i].votingCount--;      
+            for(uint i = 0; i<innovationsOfStudent[msg.sender].length; i++){
+                if(innovationsOfStudent[msg.sender][i].uniqueInnovationHash ==_uniqueInnovationHash){
+                    innovationsOfStudent[msg.sender][i].votingCount--;
+                    check++;      
+                }
             }
-        }
-        students[msg.sender].voted = false;
-        delete students[msg.sender].votedInnovationHash;
+            students[msg.sender].voted = false;
+            delete students[msg.sender].votedInnovationHash;
+            if(check==2) console.log("Student unvoted");
+            else console.log("The innovation with the specified hash does not exist");
+        } else console.log("Student already unvoted");
     }
 }
